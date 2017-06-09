b0cb7331c5f9c1aecc83a6969b65aed340bc660b
Marco Pegoraro <marco.pegoraro@gmail.com>
2017-06-09 15:29:15 +0200

    0.0.4

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -2,7 +2,7 @@
 RUNNING_CONTAINER_PATH="$PROJECT_CWD/.running"
 if [ -f "$RUNNING_CONTAINER_PATH" ];
 then
-    docker stop $(head -n 1 "$PROJECT_CWD/.running") &> null
+    docker stop $(head -n 1 "$PROJECT_CWD/.running") > /dev/null 2>&1
     rm "$RUNNING_CONTAINER_PATH"
 fi
 
--- scripts/halt.sh
+++ scripts/halt.sh
@@ -2,6 +2,6 @@
 RUNNING_CONTAINER_PATH="$PROJECT_CWD/.running"
 if [ -f "$RUNNING_CONTAINER_PATH" ];
 then
-    docker stop $(head -n 1 "$PROJECT_CWD/.running") &> null
+    docker stop $(head -n 1 "$PROJECT_CWD/.running") > /dev/null 2>&1
     rm "$RUNNING_CONTAINER_PATH"
 fi
